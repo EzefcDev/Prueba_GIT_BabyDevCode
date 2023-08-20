e345a04803a17e83f82ddaca0d77df76c4a0cbcf
EzefcDev <ezeferrauti@gmail.com>
2023-08-20 19:49:44 -0300

    cambios para el stashe

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hola desde vscode')
+console.log('Hola desde vscode');
+
+const name = 'BabyDevCode';
+const list = ['me gusta','comenta','suscribir', 'compartir'];
+const listaCosasSinCommitear = ['actualizaciones','cambio de ramas','código disponible para otra rama']
