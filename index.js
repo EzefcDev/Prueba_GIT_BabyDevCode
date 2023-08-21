4e5e8a6bd0249dc9214d30ecd131e6809012492d
EzefcDev <ezeferrauti@gmail.com>
2023-08-21 20:38:13 -0300

    error con el stash

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log('Hola desde vscode');
 
+console.log('Va a haber errores con el stash');
+
 const name = 'BabyDevCode';
 const list = ['me gusta','comenta','suscribir', 'compartir'];
 const listaCosasSinCommitear = ['actualizaciones','cambio de ramas','código disponible para otra rama']
